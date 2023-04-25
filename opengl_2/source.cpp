d70d7b085e7aa8816d80ca5079112fb4e11989c0
Chahat Kalsi <chahat.ck88@gmail.com>
2023-04-25 07:46:07 +0530

    error detection while compiling shader enabled

--- opengl_2/source.cpp
+++ opengl_2/source.cpp
@@ -64,6 +64,14 @@ int main() {
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
